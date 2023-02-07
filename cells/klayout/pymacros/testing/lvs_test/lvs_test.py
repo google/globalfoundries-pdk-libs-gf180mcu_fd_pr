17b0eafe22793c409c481d87f46a5a0e43258d1a
RehabSayed-G <rehab_hassan@mabrains.com>
2023-02-07 19:29:29 +0200

    addiing docopt string to lvs_test.py

--- cells/klayout/pymacros/testing/lvs_test/lvs_test.py
+++ cells/klayout/pymacros/testing/lvs_test/lvs_test.py
@@ -1,7 +1,28 @@
+"""
+Globalfoundries 180u lvs test.
+
+Usage:
+    lvs_test.py (--help| -h)
+    lvs_test.py (--device=<device_name>) [--thr=<thr>]
+
+Options:
+    --help -h                   Print this help message.
+    --device=<device_name>      Select your device name.
+    --thr=<thr>                 The number of threads used in run.
+"""
+from docopt import docopt
 import os
 import sys
 
-device_name = "npn_00p54x02p00"
+# arguments
+arguments = docopt(__doc__, version="PCELLS Gen.: 0.1")
+
+# No. of threads
+thrCount = (
+    os.cpu_count() * 2 if arguments["--thr"] is None else int(arguments["--thr"])
+)
+
+device_name = arguments["--device"]
 
 os.system(
     f"""
