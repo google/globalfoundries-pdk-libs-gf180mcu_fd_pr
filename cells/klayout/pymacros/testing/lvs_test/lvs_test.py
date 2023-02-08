1fe853f0136eab2488354d7bef36c09ac515c05c
RehabSayed-G <rehab_hassan@mabrains.com>
2023-02-08 09:46:16 +0200

    adding read_results section and a fail testcase

--- cells/klayout/pymacros/testing/lvs_test/lvs_test.py
+++ cells/klayout/pymacros/testing/lvs_test/lvs_test.py
@@ -18,14 +18,42 @@ import sys
 arguments = docopt(__doc__, version="PCELLS Gen.: 0.1")
 
 # No. of threads
-thrCount = (
-    os.cpu_count() * 2 if arguments["--thr"] is None else int(arguments["--thr"])
-)
+thrCount = os.cpu_count() * 2 if arguments["--thr"] is None else int(arguments["--thr"])
 
-device_name = arguments["--device"]
+device = arguments["--device"]
+test_dir = "../testcases"
 
-os.system(
-    f"""
-klayout -b -r gf180mcu.lvs -rd input=../testcases/{device_name}_pcells.gds -rd report={device_name}_pcells.lyrdb -rd schematic={device_name}_pcells.cdl -rd target_netlist=extracted_netlist_{device_name}_pcells.cir
-"""
-)
+if "npn" in device:
+    devices = ["npn_00p54x02p00", "rm1"]
+else:
+    devices = [device]
+
+res_data = []
+
+for device_name in devices:
+    print(f"running lvs for {device_name}_pcells")
+    os.system(
+        f"""
+    klayout -b -r gf180mcu.lvs -rd input={test_dir}/{device_name}_pcells.gds -rd report={device_name}_pcells.lyrdb -rd schematic={device_name}_pcells.cdl -rd target_netlist=extracted_netlist_{device_name}_pcells.cir > {test_dir}/{device_name}_pcells.log
+    """
+    )
+    print(f"reading {device_name}_pcells log")
+
+    f = open(f"{test_dir}/{device_name}_pcells.log")
+    log_data = f.readlines()
+    f.close()
+    print(log_data[-2])
+
+    if "ERROR" in log_data[-2]:
+        res_data.append((1, device_name))
+    else:
+        res_data.append((0, device_name))
+
+not_matched = []
+for dev_res in res_data:
+    if dev_res[0] == 1:
+        not_matched.append(f"{dev_res[1]}_pcells")
+
+if len(not_matched) > 0:
+    print(f"not matched pcells : {not_matched}")
+    exit(1)
