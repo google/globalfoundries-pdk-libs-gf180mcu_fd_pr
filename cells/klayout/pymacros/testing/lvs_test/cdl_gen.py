e507a70efed724d79d67e7f6f7de3e3123dc6a96
RehabSayed-G <rehab_hassan@mabrains.com>
2023-02-08 18:34:12 +0200

    removing old csv_patterns, adding cdl_gen script

---
+++ cells/klayout/pymacros/testing/lvs_test/cdl_gen.py
@@ -0,0 +1,72 @@
+"""
+Globalfoundries 180u PCells cdl Generator
+
+Usage:
+    cdl_gen.py (--help| -h)
+    cdl_gen.py (--device=<device_name>) [--thr=<thr>]
+
+Options:
+    --help -h                   Print this help message.
+    --device=<device_name>      Select your device name. Allowed devices are (bjt , diode, MIM-A, MIM-B_gfB, MIM-B_gfC , fet, cap_mos, res)
+    --thr=<thr>                 The number of threads used in run.
+"""
+
+import pandas as pd
+from docopt import docopt
+import os
+
+def cdl_gen(df,device_name) : 
+
+    cdl_f = open(f"../testcases/{device_name}_pcells.cdl","w")
+
+    cdl_f.write(
+        '''
+    # Copyright 2022 SkyWater PDK Authors
+    #
+    # Licensed under the Apache License, Version 2.0 (the "License");
+    # you may not use this file except in compliance with the License.
+    # You may obtain a copy of the License at
+    #
+    #     https://www.apache.org/licenses/LICENSE-2.0
+    #
+    # Unless required by applicable law or agreed to in writing, software
+    # distributed under the License is distributed on an "AS IS" BASIS,
+    # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    # See the License for the specific language governing permissions and
+    # limitations under the License.
+    #
+    # SPDX-License-Identifier: Apache-2.0
+
+    .SUBCKT TOP
+        '''
+    )
+    l = df["length"]
+    w = df["width"]*df["num_fing"]
+    dev = device_name
+
+
+
+    for i in range(len(df)):
+        cdl_f.write(
+            f"M{i}_{dev} s{i} g{i} d{i} Sub  {dev} W= {w[i]}u L= {l[i]}u \n   "
+        )
+
+    cdl_f.write("\n .ENDS")
+
+if __name__ == "__main__":
+
+    # arguments
+    arguments     = docopt(__doc__, version='PCELLS Gen.: 0.1')
+
+    device_name = arguments["--device"]
+    
+    # No. of threads
+    thrCount = os.cpu_count()*2 if arguments["--thr"] == None else int(arguments["--thr"])
+
+    df = pd.read_csv(f'../patterns/{device_name}_patterns.csv')
+    
+    # Calling cdl generation function 
+    cdl_gen(df=df,device_name=device_name)
+
+    
+
