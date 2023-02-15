5408859e0571c0035b51c0e8134bf604e2b46dd0
RehabSayed-G <rehab_hassan@mabrains.com>
2023-02-15 15:00:43 +0200

    modifying pcell_reg_pytest and add documentation

--- cells/klayout/pymacros/testing/lvs_test/pcell_reg_Pytest.py
+++ cells/klayout/pymacros/testing/lvs_test/pcell_reg_Pytest.py
@@ -1,49 +1,63 @@
 import pytest
 from subprocess import check_call
 import os
-import glob
-import logging
 
 
 @pytest.fixture
 def device(request):
+    """
+    Returns device argument read from command line
+    """
     return request.config.getoption("--device")
 
 
 @pytest.mark.dependency()
 def test_gds_generation(device):
+    """
+    generate gds files for device under test
 
+    Args:
+        device : name of the device under test
+    """
+
+    # gds generation command string
     call_str = f"""
     python3 ../draw_pcell.py --device={device}
     """
+
+    # assert whether generation is passed
     assert bool(check_call(call_str, shell=True)) == 0
 
 
 @pytest.mark.dependency()
 def test_cdl_generation(device):
+    """
+    generate cdl files for device under test
 
+    Args:
+        device : name of the device under test
+    """
+
+    # cdl generation command string
     call_str = f"""
     python3 cdl_gen.py --device={device}
     """
+
+    # assert whether generation is passed
     assert bool(check_call(call_str, shell=True)) == 0
-
-
-# def get_devices_list(target_device):
-
-#     file_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#     list_patt_files = glob.glob(
-#         os.path.join(file_path, "patterns", target_device, "*.csv")
-#     )
-
-#     devices = []
-#     for file_path in list_patt_files:
-#         devices.append(file_path.split("/")[-1].split("_patt")[0])
-
-#     return devices
 
 
 @pytest.mark.dependency(depends=["test_gds_generation"])
 def test_drc_run(device, device_name):
+    """
+    run drc testing for device under test testcases
+
+    Args:
+        device : name of the device under test
+        device_name : name of device testcase to be tested
+    """
+
+    # get drc rule_deck path , testing dir path
 
     file_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     root_path = os.path.dirname(
@@ -60,15 +74,24 @@ def test_drc_run(device, device_name):
     # Creating output dir
     os.makedirs(output_path, exist_ok=True)
 
+    # run drc command string
     call_str = f"""
-    python3 {drc_dir}/run_drc.py --path={test_dir}/{device_name}_pcells.gds --variant="A" > {pattern_log}
+    python3 {drc_dir}/run_drc.py --path={test_dir}/{device_name}_pcells.gds --variant="B" > {pattern_log}
     """
-    check_call(call_str, shell=True)
+    assert bool(check_call(call_str, shell=True)) == 0
 
 
 @pytest.mark.dependency(depends=["test_gds_generation", "test_cdl_generation"])
 def test_lvs_run(device, device_name):
+    """
+    run lvs testing for device under test testcases
 
+    Args:
+        device : name of the device under test
+        device_name : name of device testcase to be tested
+    """
+
+    # get lvs rule_deck path , testing dir path
     file_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
     root_path = os.path.dirname(
@@ -85,11 +108,13 @@ def test_lvs_run(device, device_name):
     # Creating output dir
     os.makedirs(output_path, exist_ok=True)
 
+    # run lvs command string
     call_str = f"""
     python3 {lvs_dir}/run_lvs.py --design={test_dir}/{device_name}_pcells.gds --net={device_name}_pcells.cdl --gf180mcu="A" > {pattern_log}
     """
     check_call(call_str, shell=True)
 
+    # read output log of lvs run
     f = open(pattern_log)
     log_data = f.readlines()
     f.close()
